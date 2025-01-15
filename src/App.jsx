02b451209bba2b689444894d30024d5d21174ab8
Petro-Herman <germanpetr24@gmail.com>
2025-01-15 23:24:45 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>TravelTrucks</h1>
+      <ul>
+        <li>Home</li>
+        <li>Catalog</li>
+      </ul>
+      <h2>Campers of your dreams</h2>
+      <p>You can find everything you want in our catalog</p>
+      <button>View Now</button>
+    </>
+  );
+}
+
+export default App;
